d4a66cd1d31896c6c330168278210384d23cbea0
Arthur Labregere <arthur.labregere@gmail.com>
2025-01-09 17:09:02 +0100

    last push

--- comptemps.bash
+++ comptemps.bash
@@ -3,9 +3,7 @@
 # Importation des fonctions de test
 source utils.bash
 
-
 verification_date(){
-
     # Vérification du nombre d'arguments
     if [ $# -ne 4 ]; then
         echo "Usage : $0 jour mois année 'date complète (ex: \"21 12 2024 15:30:45\")'" >&2
@@ -19,56 +17,67 @@ verification_date(){
     fi
 
     # Vérification de la validité de la première date (jour/mois/année)
-    if ! date -d "$3/$2/$1" &>/dev/null; then
+    if ! date -d "$3-$2-$1" &>/dev/null; then
         echo "Erreur : La date (jour/mois/année) n'est pas valide." >&2
         exit 4
     fi
 
     # Vérification de la date complète
+    DATE_COMPLETE="$4"
     if ! date -d "$(echo "$DATE_COMPLETE" | awk '{print $3"-"$2"-"$1 " " $4}')" &>/dev/null; then
         echo "Erreur : La date complète doit être valide et au format 'jour mois année heures:minutes:secondes'." >&2
         exit 4
     fi
 
+    # Date d'emprunt (passée en argument)
+    dateEmp=$(date -d "$3-$2-$1" +%Y%m%d)
+    
+    dateActu=$(date -d "$(echo "$DATE_COMPLETE" | awk '{print $3"-"$2"-"$1 " " $4}')")
 
+    # Extraction des années et des mois pour les calculs
+    anneeActuel=$(date -d "$dateActu" "+%Y")
+    anneeEmprunt=$(date -d "$dateEmp" "+%Y")
+    moisActuel=$(date -d "$dateActu" "+%m")
+    moisEmprunt=$(date -d "$dateEmp" "+%m")
 
-    # Déclaration des variables (après controle des arguments)
-    jour="$1"
-    mois="$2"
-    annee="$3"
-    # Décomposition de la date complète
-    jourActuel=$(echo "$4" | cut -d ' ' -f 1)
-    moisActuel=$(echo "$4" | cut -d ' ' -f 2)
-    anneeActuelle=$(echo "$4" | cut -d ' ' -f 3)
+    # Calcul de la différence d'années et de mois
+    diff_annee=$((anneeActuel - anneeEmprunt))
 
-
-    # Si la date saisie est postérieure ou égale à la date actuelle
-    if [ "$annee" -gt "$anneeActuelle" ] || ([ "$annee" -eq "$anneeActuelle" ] && [ "$mois" -gt "$moisActuel" ]) || ([ "$annee" -eq "$anneeActuelle" ] && [ "$mois" -eq "$moisActuel" ] && [ "$jour" -gt "$jourActuel" ]); then
+    #Teste si la date d'emprunt est dans le futur
+    if [ "$diff_annee" -lt 0 ]; then
         exit 0
+    
+    #Teste si la date d'emprunt est dans le passé
+    elif [ "$diff_annee" -gt 0 ]; then
+        diff_mois=$(( (12 - moisEmprunt) + moisActuel + (diff_annee - 1) * 12 ))
+        # Le livre a été emprunté il y a $diff_mois mois.
+        if [ "$diff_mois" -ge 12 ]; then
+            # Le livre a été emprunté il y a plus d'un an.
+            exit 1
+        elif [ "$diff_mois" -gt 1 ]; then
+            # Le livre a été emprunté il y plus d'un mois et moins d'un an.
+            exit 2
+        else
+            # Le livre a été emprunté il y a moins d'un mois.
+            exit 3
 
-    elif [ "$annee" -eq "$anneeActuelle" ] && [ "$mois" -eq "$moisActuel" ] && [ "$jour" -eq "$jourActuel" ]; then
-        exit 0
+        fi
+    else
+        diff_mois=$((moisActuel - moisEmprunt))
+
+        #Test si le livre a été emprunté dans le future
+        if [ "$diff_mois" -lt 0 ]; then
+            exit 0
+
+        #Test si le livre a été emprunté ce mois-ci
+        elif [ "$diff_mois" -eq 0 ]; then
+            # Le livre a été emprunté il y a moins d'un mois.
+            exit 3
+
+        #Test si le livre a été emprunté il y a plus d'un mois
+        else
+            # Le livre a été emprunté il y a plus d'un mois et moins d'un an.
+            exit 2
+        fi
     fi
-
-    # Si la date saisie est antérieure à la date courante de plus d'un an
-    if [ "$annee" -lt "$((anneeActuelle - 1))" ] || ([ "$annee" -eq "$((anneeActuelle - 1))" ] && [ "$mois" -lt "$moisActuel" ]) || ([ "$annee" -eq "$((anneeActuelle - 1))" ] && [ "$mois" -eq "$moisActuel" ] && [ "$jour" -lt "$jourActuel" ]); then
-        exit 1
-    fi
-
-
-    # Si la date saisie est antérieure à la date courante de plus d'un mois et moins d'un an
-    if [ "$annee" -eq "$anneeActuelle" ] && [ "$mois" -lt "$moisActuel" ] && [ "$mois" -ge "$((moisActuel - 12))" ]; then
-        echo "La date saisie est antérieure à la date courante de plus d'un mois et moins d'un an."
-        exit 2
-    fi
-
-
-
-
-
-
-
-
-
-
 }
